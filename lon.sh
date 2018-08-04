3a89154c8585b5a338e7c6da45d62ab6e4346fa1
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-08-05 04:05:29 +0800

    Update lon.sh

--- lon.sh
+++ lon.sh
@@ -122,7 +122,31 @@ sed -i $MYIP2 /etc/squid/squid.conf;
 service squid restart
 
 
-clear
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[dropbear]
+accept = 443
+connect = 127.0.0.1:3128
+END
+
+#make certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+#Configure stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+
+
 echo "========================="
 echo "Softether server configuration has been done!"
 echo " "
