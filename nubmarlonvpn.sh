3d4bfa3c4bb63e2db4f9d81a5000b25f612ba0f7
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-08-04 22:01:48 +0800

    Create nubmarlonvpn.sh

---
+++ nubmarlonvpn.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+#
+# 
+# Mod by Nub Marlon
+# ==================================================
+
+
+
+HOST=""
+SERVER_PASSWORD=""
+USER=""
+HUB=""
+SE_PASSWORD=""
+
+HOST=${HOST}
+HUB=${HUB}
+USER_PASSWORD=${SERVER_PASSWORD}
+SE_PASSWORD=${SE_PASSWORD}
+
+echo -n "Enter Server IP: "
+read HOST
+echo -n "Set Virtual Hub: "
+read HUB
+echo -n "Set ${HUB} hub username: "
+read USER
+echo ""
+read -s -p "Set SE Server password: " SE_PASSWORD
+echo ""
+echo " "
+echo "Now sit back and wait until the installation finished."
+echo " "
+
+sudo apt-get -y update && sudo apt-get -y upgrade && apt-get install expect -y
+
+
+wget http://www.softether-download.com/files/softether/v4.27-9668-beta-2018.05.29-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
+sudo tar xvzf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
+rm -rf softether-vpnserver-v4.27-9668-beta-2018.05.29-linux-x64-64bit.tar.gz
+
+sudo apt-get install checkinstall build-essential -y	
+
+
+
+cd vpnserver && expect -c 'spawn make; expect number:; send 1\r; expect number:; send 1\r; expect number:; send 1\r; interact'
+
+cd ..
+
+sudo mv vpnserver/ /usr/local && chmod 600 * /usr/local/vpnserver/ && chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
+
+echo '#!/bin/sh
+# description: SoftEther VPN Server
+### BEGIN INIT INFO
+# Provides:          vpnserver
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: softether vpnserver
+# Description:       softether vpnserver daemon
+### END INIT INFO
+DAEMON=/usr/local/vpnserver/vpnserver
+LOCK=/var/lock/subsys/vpnserver
+test -x $DAEMON || exit 0
+case "$1" in
+start)
+$DAEMON start
+touch $LOCK
+;;
+stop)
+$DAEMON stop
+rm $LOCK
+;;
+restart)
+$DAEMON stop
+sleep 3
+$DAEMON start
+;;
+*)
+echo "Usage: $0 {start|stop|restart}"
+exit 1
+esac
+exit 0' > /etc/init.d/vpnserver
+###
+chmod 755 /etc/init.d/vpnserver && /etc/init.d/vpnserver start
+update-rc.d vpnserver defaults
+
+
+HOST=${HOST}
+HUB_PASSWORD=${SE_PASSWORD}
+USER_PASSWORD=${SERVER_PASSWORD}
+
+TARGET="/usr/local/"
+
+sleep 2
+${TARGET}vpnserver/vpncmd localhost /SERVER /CMD ServerPasswordSet ${SE_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD HubCreate ${HUB} /PASSWORD:${HUB_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD UserCreate ${USER} /GROUP:none /REALNAME:none /NOTE:none
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD UserPasswordSet ${USER} /PASSWORD:${USER_PASSWORD}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD IPsecEnable /L2TP:yes /L2TPRAW:yes /ETHERIP:no /PSK:vpn /DEFAULTHUB:${HUB}
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD HubDelete DEFAULT
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /HUB:${HUB} /CMD SecureNatEnable
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD VpnOverIcmpDnsEnable /ICMP:yes /DNS:yes
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 443
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 992
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 1194
+${TARGET}vpnserver/vpncmd localhost /SERVER /PASSWORD:${SE_PASSWORD} /CMD ListenerCreate 5555
+
+clear
+echo "Softether server configuration has been done!"
+echo " "
+echo "Host: ${HOST}"
+echo "Virtual Hub: ${HUB}"
+echo "Port: 443, 992, 1194, 5555"
+echo "Username: ${USER}"
+echo "Password: ${SERVER_PASSWORD}"
+echo "Server Password: ${SE_PASSWORD}"
