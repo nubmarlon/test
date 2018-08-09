37ddbaf64d8fd96fd4d5fb47bc17f543040ff4a0
nubmarlon <41515999+nubmarlon@users.noreply.github.com>
2018-08-10 06:24:56 +0800

    Create txting.sh

---
+++ txting.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# 
+# Mod by Nub Marlon
+# ==================================================
+clear
+clear
+
+echo "                                                      "
+echo "                                                      "
+echo "      _|             _|_|_|_|_|      _|      _|       "
+echo "      _|             _|      _|      _|_|    _|       "
+echo "      _|             _|      _|      _|  _|  _|       "
+echo "      _|             _|      _|      _|    _|_|       "
+echo "      _|_|_|_|_|     _|_|_|_|_|      _|      _|       "
+echo "                                                      "
+echo "                                                      "
+echo "                     AUTO SCRIPT                      "
+echo "                                                      "
+echo " "
